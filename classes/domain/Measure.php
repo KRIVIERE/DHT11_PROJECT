45baadac6abf57a6dad9fa9cf7883b15ed960a78
KRIVIERE <kvin_riviere@yahoo.fr>
2018-02-01 14:00:37 +0100

    Début d'écriture des configurations

---
+++ classes/domain/Measure.php
@@ -0,0 +1,13 @@
+<?php
+class Measure {
+    public $id;
+    public $datetime;
+    public $temperature;
+    public $humidite;
+
+    public function __construct($datetime, $temperature, $humidite) {
+        $this->datetime = $datetime;
+        $this->temperature = $temperature;
+        $this->humidite = $humidite;
+    }
+}
---
+++ inc/config.inc.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+    'db.host' => 'localhost',
+    'db.port' => 3306,
+    'db.username' => 'dht11',
+    'db.password' => 'Sil2d1TXlyAVhHqD',
+    'db.name' => 'dht11'
+);

eef6ad6dcb661040a3de58bacc7e9f1f871a58cd
KRIVIERE <kvin_riviere@yahoo.fr>
2018-02-15 15:30:41 +0100

    Suppression doublons dans la bdd

--- tests/MeasureDaoTest.php
+++ tests/MeasureDaoTest.php
@@ -45,23 +45,19 @@ class MeasureDaoTest extends TestCase
     {
         $measures = $this->measureDao->readAllMeasure();
 
-        $this->assertEquals(4, count($measures));
+        $this->assertEquals(3, count($measures));
 
         $this->assertEquals("1879-03-14 00:00:00", $measures[0]->datetime);
         $this->assertEquals(20, $measures[0]->temperature);
         $this->assertEquals(25, $measures[0]->humidite);
 
         $this->assertEquals("1879-03-14 00:00:00", $measures[1]->datetime);
-        $this->assertEquals(20, $measures[1]->temperature);
-        $this->assertEquals(25, $measures[1]->humidite);
+        $this->assertEquals(46, $measures[1]->temperature);
+        $this->assertEquals(46, $measures[1]->humidite);
 
         $this->assertEquals("1879-03-14 00:00:00", $measures[1]->datetime);
-        $this->assertEquals(46, $measures[2]->temperature);
-        $this->assertEquals(46, $measures[2]->humidite);
-
-        $this->assertEquals("1879-03-14 00:00:00", $measures[1]->datetime);
-        $this->assertEquals(23, $measures[3]->temperature);
-        $this->assertEquals(23, $measures[3]->humidite);
+        $this->assertEquals(23, $measures[2]->temperature);
+        $this->assertEquals(23, $measures[2]->humidite);
     }
 
     public function testReadMeasureById()
